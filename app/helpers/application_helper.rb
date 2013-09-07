2cac024ad118d47163936a770a5b7798dc073b4f
Tim Cort <timcort@gmail.com>
2013-09-07 13:12:21 +1000

    fill in layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+	# Returns the full title on a per-page basis
+
+	def full_title(page_title)
+		base_title = "Underdog Upstart"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
+
+	def price_on_button(price)
+		free = "Free"
+		if @product.price == 0.0
+			free
+		else
+			"#{price}"
+		end
+	end
 end
